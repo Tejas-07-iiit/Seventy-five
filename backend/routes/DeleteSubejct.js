fe6cf16650af68d23b64214da6c5a5f6a318b5d4
Tejas-07-iiit <tejas23106@gmail.com>
2026-02-06 19:37:20 +0530

    attendacne fix

--- backend/routes/DeleteSubejct.js
+++ backend/routes/DeleteSubejct.js
@@ -2,14 +2,21 @@ const express =  require("express")
 const Subject = require("../models/subject.model")
 const Auth = require("../Middleware/Auth")
 const router = express()
+const Attendance = require("../models/Attendance.model")
 
 
 router.delete("/dsubject" , Auth ,async (req,res)=>{
     try {
         const scode = req.body.scode;
-        const response = await Subject.deleteOne({scode : scode});
+        console.log(req.user)
+        const s1  = await Subject.findOne({scode:scode, userid : req.user.userId}) 
+        console.log(s1)
+        const atd = await Attendance.deleteOne({scode : s1._id , userid : req.user.userId});
+        const s2 = await Subject.deleteOne({scode : scode});
+        // console.log(atd)
+        
 
-        if(response){
+        if(s1 && s2){
             res.status(200).json({message : "Subject Deleted successfully"})
         }
 
